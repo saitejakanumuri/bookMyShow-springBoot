3b5e5eb6f8f80a463b21c3e961d5391280172799
saiteja kanumuri <saiteja@thinkvidya.com>
2026-02-28 14:00:05 +0530

    add property REACT_APP_API_URL

--- client/src/calls/index.js
+++ client/src/calls/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // In client/src/calls/index.js
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8081/api", //process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
         "Content-Type": "application/json"
     }
